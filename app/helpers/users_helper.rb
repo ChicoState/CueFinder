a246396ead51620d4c39849024a24030eb752552
Mark Moniz <megglymark@gmail.com>
2015-07-24 16:11:58 -0700

    Moving Username/Password generation to script

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,47 @@
 module UsersHelper
 end
+
+def create_all
+  @animals = %w(camel 
+               chimp 
+               horse 
+               llama
+               moose 
+               mouse
+               puppy
+               rhino
+               zebra
+               sloth
+               sheep
+               beaver
+               coyote
+               jaguar)
+
+  @colors = %w(black
+              white
+              red
+              blue
+              yellow
+              green
+              purple
+              pink
+              brown
+              orange)
+
+  @username = Array.new
+  @password = Array.new
+  output = File.open("tmp/users.txt", "a")
+
+  (0..100).each do
+    
+    @username << @colors.shuffle.first << @animals.shuffle.first << rand(9) << rand(9)
+    @password << rand(100000..999999)
+
+    output << @username.join << "\t" << @password.join << "\n"
+
+    @password.clear
+    @username.clear
+  end
+end
+
+create_all
