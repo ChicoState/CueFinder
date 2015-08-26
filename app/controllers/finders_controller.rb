f277a4d742c80c707e39df528df64158826ed2bf
Mark Moniz <megglymark@gmail.com>
2015-08-26 10:42:39 -0700

    Adding join table finder_items

--- app/controllers/finders_controller.rb
+++ app/controllers/finders_controller.rb
@@ -18,14 +18,28 @@ class FindersController < ApplicationController
     end
   end
 
-  def show(id)
-    @finder = Finder.find_by(id: 4)
+  def show
+    @finder = Finder.find(params[:id])
+  end
+
+  def edit
+    @finder = Finder.find(params[:id])
+    @items = Item.all
+  end
+
+  def update
+    @finder = Finder.find(params[:id])
+    if @finder.update(finder_params)
+      redirect_to(:back)
+    else
+      redirect_to(:back)
+    end
   end
 
   private
 
     def finder_params
-      params.require(:finder).permit(:question, :noneEnabled, :multipleChoice, :timed)
+      params.require(:finder).permit(:question, :noneEnabled, :multipleChoice, :timed, :item_ids=>[])
     end
 end
 
