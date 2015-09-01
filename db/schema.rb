a113e3f1a1e42e0bfacbc571e37fda42b9f601df
Mark Moniz <megglymark@gmail.com>
2015-08-31 17:53:19 -0700

    Adding current question to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150827071026) do
+ActiveRecord::Schema.define(version: 20150901005003) do
 
   create_table "finders", force: :cascade do |t|
     t.string   "question",       limit: 255
@@ -104,10 +104,11 @@ ActiveRecord::Schema.define(version: 20150827071026) do
   add_index "selections", ["user_id"], name: "index_selections_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        limit: 255
-    t.string   "password_digest", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "username",         limit: 255
+    t.string   "password_digest",  limit: 255
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.integer  "current_question", limit: 4
   end
 
 end
